e8c83cd1d44a60f35173bfef45a54e08600f7019
Patricia Manzanas <pmanzanastomas@gmail.com>
2023-07-21 18:23:27 +0200

    Ejercicio 2 N2 complteto

--- Nivell_2/E2_N2.php
+++ Nivell_2/E2_N2.php
@@ -6,17 +6,27 @@ $clase1= array(
     "Anna" =>array(4,8,2,3,5),
     "Carlos" => array(5,5,6,6,7)
 );
+
 print_r($clase1) ;
 echo "<br>"; 
 
  foreach($clase1 as $alumne=>$notas){
     echo "<br>"; 
-    echo"L'alumne ".$alumne. ", te una mitjana de ".array_sum($notas)/count($notas);   
+    $total=array_sum($notas)/count($notas);
+    echo"L'alumne ".$alumne. ", te una mitjana de ".$total;   
     echo "<br>"; 
+  
+}
+        
+$total_notas =0;
+
+foreach ($clase1 as $alumne=> $notas) {
+    
+    $total_notas += array_sum($notas);
     
 }
+$mediaClase = $total_notas / (count($notas)*count($clase1));
 
-
-
+echo "La puntuacion media de la clase es: " . $mediaClase ;
 
 ?>
